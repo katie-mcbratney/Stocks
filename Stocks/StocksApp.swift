d09cd6c77520bb315fa79138934993ada11f44e6
katie-mcbratney <88895553+katie-mcbratney@users.noreply.github.com>
2021-08-23 11:04:15 -0700

    Initial Commit

---
+++ Stocks/StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp.swift
+//  Stocks
+//
+//  Created by Katie McBratney on 8/23/21.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
